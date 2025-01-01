815169fe67cbf36cffa401952656ad0ff6391a27
ascreendarkly <143303168+ascreendarkly@users.noreply.github.com>
2025-01-01 17:27:08 +1100

    Monthly calculator more generic

--- shared-calculator/script.js
+++ shared-calculator/script.js
@@ -1,11 +1,11 @@
     function calculateShares() {
             const startingBalance = document.getElementById('available-balance').value;
-            const ashleysShare = document.getElementById('ashleys-share');
-            const nicksShare = document.getElementById('nicks-share');
+            const fortyFiveShare = document.getElementById('forty-five-share');
+            const fiftyFiveShare = document.getElementById('fifty-five-share');
 
-            const ashleysAmount = (((10000 - startingBalance) * 0.45).toFixed(2));
-            const nicksAmount = (((10000 - startingBalance) * 0.55).toFixed(2));
+            const fortyFiveAmount = (((10000 - startingBalance) * 0.45).toFixed(2));
+            const fiftyFiveAmount = (((10000 - startingBalance) * 0.55).toFixed(2));
 
-            ashleysShare.innerHTML= `$${ashleysAmount}`;
-            nicksShare.innerHTML = `$${nicksAmount}`;
+            fortyFiveShare.innerHTML= `$${fortyFiveAmount}`;
+            fiftyFiveShare.innerHTML = `$${fiftyFiveAmount}`;
         }       
